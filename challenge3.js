6111492712d52255d3cea76a48c8fcc71dbbbc98
unknown <baibouamohamed@gmail.com>
2026-09-17 12:52:55 +0200

    the theard challenge

---
+++ challenge3.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+const km = prompt('Entré  la distance en kilomètres et la transforme en yards : ');
+let y = km * 1093.61 ; 
+console.log("La distance en km et la transforme en yards est : ",y);
